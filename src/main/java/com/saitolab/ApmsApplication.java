c2b5225679aa5dd34adb415feac5a56a7e34b71c
XIA WEIYI <sdakny@gmail.com>
2023-04-08 16:52:35 +0900

    Initial commit

---
+++ src/main/java/com/saitolab/ApmsApplication.java
@@ -0,0 +1,13 @@
+package com.saitolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saitolab/ApmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saitolab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
